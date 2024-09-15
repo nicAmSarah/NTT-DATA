8506efe287d90ae16d190d727702bd40ca76996a
Sarah Nicacio <nicaciosarah085@gmail.com>
2024-09-15 19:22:34 -0300

    feat:part-1 of strings

--- string1/strinf1.py
+++ string1/strinf1.py
@@ -1,3 +1,17 @@
 nome = "Sarah"
 
-print(nome.upper)
+print(nome.upper())  #maiusculo
+print(nome.lower()) #minusculo
+print(nome.title()) #primeiraMaiuscula
+
+texto = "  ola    "
+print(nome)
+print(texto.strip())  #apagar os espaços
+print(texto.lstrip())   #apagar os espaços da esquerda
+print(texto.rstrip())   #apagar os espaços da direita
+
+palavra = "NumSei"
+
+print(palavra.center(14)) #centralizar a palavra
+print(palavra.center(14,"x")) #centralizar a palavra incluindo caracteres
+print("-".join(palavra))    #incluir nas letras o caractere pedido
